7f48ca3987e7aff168dd7974729ec87469232cfa
izayaaaaaaa <izaya.orihara0011@gmail.com>
2023-04-11 10:46:49 +0800

    Frontend Routes + React Redux Additional Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import { HomePage } from './scenes/HomePage';
+import { LoginPage } from './scenes/LoginPage';
+import { ProfilePage } from './scenes/ProfilePage';
 
 function App() {
-  return (
-    <div className="app">
-      
-    </div>
-  );
-}
-
-export default App;
+    return (
+        <div className="app">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<LoginPage />} />
+                    <Route path="/home" element={<HomePage />} />
+                    <Route path="/profile/:userId" element={<ProfilePage />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+        );
+    }
+    
+    export default App;
+    
---
+++ src/scenes/navbar/index.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return (
+        <div>
+            <h1>Navbar</h1>
+            <p>This is the navbar</p>
+        </div>
+    );
+};
+
+export default Navbar;
