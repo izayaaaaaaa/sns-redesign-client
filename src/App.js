0b23b11c777236a4ee058888d27c674cb1e83e93
izayaaaaaaa <izaya.orihara0011@gmail.com>
2023-04-11 23:13:59 +0800

    coded Register, Login, and Form Pages

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { themeSettings } from './theme';
 function App() {
     const mode = useSelector((state) => state.mode); // grab initial state
     const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]); 
+    const isAuth = Boolean(useSelector((state) => state.token));
 
     return (
         <div className="app">
@@ -20,14 +21,20 @@ function App() {
                     <CssBaseline /> 
                     <Routes>
                         <Route path="/" element={<LoginPage />} />
-                        <Route path="/home" element={<HomePage />} />
-                        <Route path="/profile/:userId" element={<ProfilePage />} />
+                        <Route 
+                            path="/home" 
+                            element={isAuth ? <HomePage /> : <Navigate to="/" />} 
+                        />
+                        <Route 
+                            path="/profile/:userId" 
+                            element={isAuth ? <ProfilePage /> : <Navigate to="/" />} 
+                        />
                     </Routes>
                 </ThemeProvider>
             </BrowserRouter>
         </div>
-        );
-    }
+    );
+}
     
-    export default App;
+export default App;
     
